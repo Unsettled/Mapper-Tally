6e988682d7da35990855becb7f36fc2fd07da409
Zumochi <zumpyzum@gmail.com>
2015-05-16 04:44:54 +0200

    Initial commit.

---
+++ Config.sample.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Copy this file to Config.php and edit the variables
+ */
+
+define('DB_NAME', "evewspace");
+define('DB_DSN', "mysql:unix_socket=/var/run/mysqld/mysqld.sock; dbname=" . DB_NAME);
+define('DB_USER', "maptool");
+define('DB_PASS', "password");
+define('SLACK_URL', "https://hooks.slack.com/services/xxxxxxxxx/xxxxxxxxx/xxxxxxxxxxxxxxxxxxxxxxxx");
