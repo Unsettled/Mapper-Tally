cf44df5fa92eeb3948630277f7f8aaac85b8acb5
Zumochi <zumpyzum@gmail.com>
2015-05-16 18:36:45 +0200

    Added option to set start and end dates by GET. var_dump output by default, $_GET['slack'] must evaluate to true to send.

--- public/MapperTally.php
+++ public/MapperTally.php
@@ -9,7 +9,7 @@ require __DIR__ . '/../Config.php';
 class Tally
 {
     private $DBH;
-    private $DateNow, $DateLast;
+    private $DateStart, $DateEnd;
 
     function __construct()
     {
@@ -17,15 +17,47 @@ class Tally
             $this->DBH = new PDO(DB_DSN, DB_USER, DB_PASS);
             $this->DBH->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         } catch (PDOException $e) {
-            exit("Error connecting to database: " . $e->getMessage());
+            exit("Error connecting to database: " . "<pre>" . $e->getMessage() . "</pre>");
         }
     }
 
     function SetDates()
     {
-        $this->DateNow = date('Y-m-d');
-        $last = strtotime('-1 month');
-        $this->DateLast = date('Y-m-d', $last);
+        if (!empty($_GET['start'])) {
+            $input = filter_input(INPUT_GET, 'start', FILTER_SANITIZE_NUMBER_INT);
+            $year = substr($input, 0, 4);
+            $month = substr($input, 5, 2);
+            $day = substr($input, 8, 2);
+
+            if (checkdate($month, $day, $year)) {
+                $this->DateStart = $input;
+            } else {
+                echo "<pre>Error validating start date input.<br>";
+                echo "Make sure you use YYYY-MM-DD for the date format.";
+                exit;
+            };
+
+            if (!empty($_GET['end'])) {
+                $input = filter_input(INPUT_GET, 'end', FILTER_SANITIZE_NUMBER_INT);
+                $year = substr($input, 0, 4);
+                $month = substr($input, 5, 2);
+                $day = substr($input, 8, 2);
+
+                if (checkdate($month, $day, $year)) {
+                    $this->DateEnd = $input;
+                } else {
+                    echo "<pre>Error validating end date input.<br>";
+                    echo "Make sure you use YYYY-MM-DD for the date format.";
+                    exit;
+                }
+            } else {
+                exit("You need to provide an end date if you specify a start date!");
+            }
+        } else {
+            $this->DateEnd = date('Y-m-d');
+            $last = strtotime('-1 month');
+            $this->DateStart = date('Y-m-d', $last);
+        }
     }
 
     function ExecuteQuery()
@@ -34,12 +66,12 @@ class Tally
             "SELECT user.username, COUNT(1) AS log_count FROM Map_maplog log
             JOIN account_ewsuser user ON user.id = log.user_id
             WHERE log.action LIKE 'Added system%'
-            AND (log.timestamp >= :DateLast AND log.timestamp <= :DateNow)
-            GROUP BY user.id ORDER BY log_count
-            DESC LIMIT 10"
+            AND (log.timestamp >= :DateEnd AND log.timestamp <= :DateStart)
+            GROUP BY user.id ORDER BY log_count DESC
+            LIMIT 5"
         );
-        $STH->bindParam(':DateLast', $this->DateLast);
-        $STH->bindParam(':DateNow', $this->DateNow);
+        $STH->bindParam(':DateStart', $this->DateEnd);
+        $STH->bindParam(':DateEnd', $this->DateStart);
         $STH->setFetchMode(PDO::FETCH_ASSOC);
 
         if ($STH->execute()) {
@@ -57,7 +89,7 @@ class Tally
         foreach ($data as $key => $person) {
             $slackFormat[$key] = array(
                 "title" => $person['username'],
-                "value" => $person['log_count'],
+                "value" => $person['log_count'] . " systems added",
             );
         }
 
@@ -65,7 +97,7 @@ class Tally
             "username"    => "Scanning Tally Bot",
             "attachments" => array(array(
                 "fallback" => "This month's top scanner: " . $data[0]['username'] . " with " . $data[0]['log_count'] . " systems added!",
-                "pretext"  => "Top Scanners for period " . $this->DateLast . " - " . $this->DateNow . ".",
+                "pretext"  => "Top Scanners for period: " . $this->DateStart . " to " . $this->DateEnd . ".",
                 "fields"   => $slackFormat,
             )),
         );
@@ -82,8 +114,15 @@ class Tally
     }
 }
 
+
 $Tally = new Tally();
 $Tally->SetDates();
 $out = $Tally->ExecuteQuery();
 
-$Tally->SendToSlack($out);
+if (!empty($_GET['slack'])) {
+    $slack = filter_input(INPUT_GET, 'slack', FILTER_VALIDATE_BOOLEAN);
+
+    if ($slack) {
+        $Tally->SendToSlack($out);
+    } else var_dump($out);
+} else var_dump($out);
